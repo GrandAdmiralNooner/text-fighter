54523867f251378bbfe2b93aef546bee949bf117
GrandAdmiralNooner <93213346+GrandAdmiralNooner@users.noreply.github.com>
2023-08-10 21:39:24 -0700

    - Update mirrorMatch.h
    - Updated the mirror sentence

--- mirrorMatch.h
+++ mirrorMatch.h
@@ -5,7 +5,7 @@ int mirrorMatch(std::string x, std::string y)
     if (x == y)
     {
         std::cout << "That match is not allowed. \n";
-        std::cout << "You will need to choosing your opponent again. \n";
+        std::cout << "You will need to choose your opponent again. \n";
         return -1;
     } else {
         std::cout << "This will be an interesting battle. Good luck. \n";
